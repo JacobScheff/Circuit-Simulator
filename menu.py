7c3bf545c021b26336f94a4f5e87f32a25377b86
Snowplou <Snowplou01@gmail.com>
2025-04-23 13:06:45 -0700

    made menu precompute a lot of stuff

--- menu.py
+++ menu.py
@@ -9,21 +9,29 @@ class Menu:
         self.selected_option = 0
         self.font = pygame.font.Font(None, 36)
 
-    def draw(self):
-        background_color = (120, 120, 120)
-        text_color = (255, 255, 255)
-        text_to_border_padding = 5
-        text_line_spacing = 40
-        max_text_x = -99999
+        self.background_color = (120, 120, 120)
+        self.text_color = (255, 255, 255)
+        self.text_to_border_padding = 5
+        self.text_line_spacing = 40
+        self.max_text_x = -99999
+        self.text_surfaces = []
+        self.text_rects = []
 
+        # Precalculate text surfaces
         for i, option in enumerate(self.options):
-            text_surface = self.font.render(option, True, text_color)
-            text_rect = text_surface.get_rect(center=(self.position[0] + text_to_border_padding, self.position[1] + i * text_line_spacing))
-            size_x = text_surface.get_size()[0]
-            max_text_x = max(max_text_x, size_x)
-            text_rect = text_surface.get_rect(center=(self.position[0] + size_x / 2, self.position[1] + i * text_line_spacing))
+            text_surface = self.font.render(option, True, self.text_color)
+            surface_size = text_surface.get_size()
+            self.max_text_x = max(self.max_text_x, surface_size[0])
+            text_rect = text_surface.get_rect(center=(self.position[0] + surface_size[0] / 2, self.position[1] + i * self.text_line_spacing))
+            self.text_surfaces.append(text_surface)
+            self.text_rects.append(text_rect)
+            
 
-            # Draw text
+    def draw(self):
+        # Draw the text
+        for i in range(len(self.text_rects)):
+            text_rect = self.text_rects[i]
+            text_surface = self.text_surfaces[i]
             self.screen.blit(text_surface, text_rect)
 
         # Draw background behind entire menu
