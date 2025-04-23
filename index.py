82937eede54a8ebfac1c35f65ee251ce7811e4c0
Snowplou <Snowplou01@gmail.com>
2025-04-23 11:52:47 -0700

    added basic pygame code

--- index.py
+++ index.py
@@ -1,1 +1,22 @@
-print("Hello world!")
+import pygame
+
+SCREEN_SIZE = (1200, 600)
+
+pygame.init()
+screen = pygame.display.set_mode(SCREEN_SIZE)
+pygame.display.set_caption("Circuit Simualtor")
+clock = pygame.time.Clock()
+FPS = 60
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0, 0, 0))
+
+    pygame.display.flip()
+    clock.tick(FPS)
+
+pygame.quit()
