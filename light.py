33f01baee1d701e193a48ec162fe269e4df7d720
Snowplou <Snowplou01@gmail.com>
2025-04-29 12:38:50 -0700

    added wire connectors for lights

--- light.py
+++ light.py
@@ -6,28 +6,35 @@ class Light:
     def __init__(self, screen, pos):
         self.screen = screen
         self.pos = pos
-        self.size = 30
+        self.radius = 30
         self.state = False
+        self.wire_connectors = [(pos[0] - self.radius, pos[1]), (pos[0] + self.radius, pos[1])]
         self.deleted = False # Whether to delete the element next frame
 
     def draw(self):
         # Draw the light bulb
         color = (255, 255, 0) if self.state else (100, 100, 100)
-        pygame.draw.circle(self.screen, color, self.pos, self.size)
+        pygame.draw.circle(self.screen, color, self.pos, self.radius)
+
+        # Draw the wire connectors
+        pygame.draw.circle(self.screen, (0, 0, 255), self.wire_connectors[0], 5)
+        pygame.draw.circle(self.screen, (0, 0, 255), self.wire_connectors[1], 5)
 
     def set_pos(self, pos):
         # Set the position of the light bulb
         self.pos = pos
+        self.wire_connectors[0] = (pos[0] - self.radius, pos[1])
+        self.wire_connectors[1] = (pos[0] + self.radius, pos[1])
 
     # Return True if the light bulb was clicked
     def handle_click(self, mouse_pos):
-        if math.hypot(self.pos[0] - mouse_pos[0], self.pos[1] - mouse_pos[1]) < self.size:
+        if math.hypot(self.pos[0] - mouse_pos[0], self.pos[1] - mouse_pos[1]) < self.radius:
             return True
         return False
 
     def handle_menu_create(self, mouse_pos):
         # Check if the input box is clicked
-        if math.hypot(self.pos[0] - mouse_pos[0], self.pos[1] - mouse_pos[1]) < self.size:
+        if math.hypot(self.pos[0] - mouse_pos[0], self.pos[1] - mouse_pos[1]) < self.radius:
             def delete_input():
                 self.deleted = True
                 return True # Close the menu after deleting the input
