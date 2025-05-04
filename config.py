07b13d7912fe941c8643d36ab87487a58fd2051d
Snowplou <snowplou01@gmail.com>
2025-05-03 18:05:27 -0700

    added display for or gate

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import pygame
+
 SCREEN_SIZE = (1200, 600)
 GRID_SIZE = 25 # Grid size when grid is enabled
 FPS = 60
@@ -6,3 +8,6 @@ WIRE_RADIUS = 5 # Radius of the wire
 WIRE_CONNECOR_RADIUS = 5 # Radius of the wire connector
 WIRE_ON_COLOR = (255, 255, 255) # Color of the wire when on
 WIRE_OFF_COLOR = (100, 100, 100) # Color of the wire when off
+
+### --- Symbols --- ###
+OR_GATE_IMAGE = pygame.image.load("symbols/or_gate.tiff")
--- or_gate.py
+++ or_gate.py
@@ -7,20 +7,18 @@ class OrGate:
     def __init__(self, screen, pos):
         self.screen = screen
         self.pos = pos
-        self.radius = 30
         self.state = False
-        self.wire_connectors = [((pos[0] - self.radius, pos[1]), True), ((pos[0] + self.radius, pos[1]), False)] # (pos, is_input)
+        self.wire_connectors = [] # (pos, is_input)
         self.input_wires = [] # List of wires that connect to this light
         self.deleted = False # Whether to delete the element next frame
 
     def draw(self):
-        # Draw the light bulb
-        color = (255, 255, 0) if self.state else (100, 100, 100)
-        pygame.draw.circle(self.screen, color, self.pos, self.radius)
+        # Draw OR_GATE_IMAGE
+        self.screen.blit(OR_GATE_IMAGE, (self.pos[0] - OR_GATE_IMAGE.get_width() // 2, self.pos[1] - OR_GATE_IMAGE.get_height() // 2))
 
         # Draw the wire connectors
-        pygame.draw.circle(self.screen, (0, 0, 255), self.wire_connectors[0][0], WIRE_CONNECOR_RADIUS)
-        pygame.draw.circle(self.screen, (0, 0, 255), self.wire_connectors[1][0], WIRE_CONNECOR_RADIUS)
+        for connector in self.wire_connectors:
+            pygame.draw.circle(self.screen, (0, 0, 255), connector[0], WIRE_CONNECOR_RADIUS)
 
     def update(self):
         self.state = False # Reset the state of the light bulb
@@ -41,18 +39,15 @@ class OrGate:
     def set_pos(self, pos):
         # Set the position of the light bulb
         self.pos = pos
-        self.wire_connectors[0] = ((pos[0] - self.radius, pos[1]), True)
-        self.wire_connectors[1] = ((pos[0] + self.radius, pos[1]), False)
 
     # Return True if the light bulb was clicked
     def handle_click(self, mouse_pos):
-        if math.hypot(self.pos[0] - mouse_pos[0], self.pos[1] - mouse_pos[1]) < self.radius:
-            return True
+        # TODO
         return False
 
     def handle_menu_create(self, mouse_pos):
         # Check if the or gate is clicked
-        if math.hypot(self.pos[0] - mouse_pos[0], self.pos[1] - mouse_pos[1]) < self.radius:
+        if False: # TODO: Check if clicked
             def delete_input():
                 self.deleted = True
                 return True # Close the menu after deleting the input
